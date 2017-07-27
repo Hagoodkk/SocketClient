cde290ea14db0d56286f7b33f7df65b84d1bf08e
Kyle Hagood <Hagood.Kyle@gmail.com>
2017-07-27 16:47:01 -0400

    Messages sending now

--- src/com/example/project/ChatWindow/ChatWindowController.java
+++ src/com/example/project/ChatWindow/ChatWindowController.java
@@ -1,10 +1,6 @@
 package com.example.project.ChatWindow;
 
 import com.example.project.SessionManager.SessionManager;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
@@ -13,7 +9,6 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.util.Duration;
 
 import java.io.*;
 import java.net.Socket;
@@ -41,11 +36,23 @@ public class ChatWindowController {
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-            
-
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
+
+        new Thread(() -> {
+            try {
+                while (!Thread.interrupted()) {
+                    String line = in.readLine();
+                    if (line == null) {
+                        break;
+                    }
+                    System.out.println(line);
+                }
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        });
     }
 
     public void handleMouseClick(MouseEvent mouseEvent) {
@@ -68,5 +75,10 @@ public class ChatWindowController {
         if (clientSocket.isClosed()) System.out.println("Closed.");
         input_field.clear();
         input_field.requestFocus();
+        try {
+            System.out.println(in.readLine());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
